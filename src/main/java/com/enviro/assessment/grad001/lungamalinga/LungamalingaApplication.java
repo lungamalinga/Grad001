0532263d1b78931a51928a2464d55cd8de262c45
lumalin022 <lumalin022@student.wethinkcode.co.za>
2025-01-28 21:06:27 +0200

    setup db on properties file

--- src/main/java/com/enviro/assessment/grad001/lungamalinga/LungamalingaApplication.java
+++ src/main/java/com/enviro/assessment/grad001/lungamalinga/LungamalingaApplication.java
@@ -2,12 +2,14 @@ package com.enviro.assessment.grad001.lungamalinga;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class LungamalingaApplication {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
+
 		SpringApplication.run(LungamalingaApplication.class, args);
+
 	}
 }
