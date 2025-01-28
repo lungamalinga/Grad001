52ccd7b35b3220b313438b1d04ed56ed3ad5a7b7
lumalin022 <lumalin022@student.wethinkcode.co.za>
2025-01-28 20:38:27 +0200

    springboot skeleton code

---
+++ src/main/java/com/enviro/assessment/grad001/lungamalinga/LungamalingaApplication.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.lungamalinga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LungamalingaApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		SpringApplication.run(LungamalingaApplication.class, args);
+	}
+}
