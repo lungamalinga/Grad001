7af8c3bcf30a379ccd7b2f5b531327bb802702a5
lumalin022 <lumalin022@student.wethinkcode.co.za>
2025-01-28 19:28:55 +0200

    initial commit

---
+++ src/main/java/com/enviro/assessment/grad001/lungamalinga/Main.java
@@ -0,0 +1,8 @@
+package com.enviro.assessment.grad001.lungamalinga;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+    }
+}
